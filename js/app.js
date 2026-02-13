771033874ac422f4efd7580f2e850b7d2da9403d
Alexandre Wahl <alexandre.wahl12@gmail.com>
2026-02-13 16:55:48 -0300

    fix: botao adicionar e limpar agora apagam a quantidade informada no campo

--- js/app.js
+++ js/app.js
@@ -6,6 +6,9 @@ function adicionar(){
     let carrinho = document.getElementById('lista-produtos');
     let listaProdutos = carrinho.querySelector('.carrinho__produtos__produto');
     let selecionarTotal = document.getElementById('valor-total');
+    let quantidadeSelecionadaTexto = document.getElementById('quantidade');
+        
+    quantidadeSelecionadaTexto.value = '';
 
 
     listaProdutos.innerHTML += `<br><span class="texto-azul">${quantidadeSelecionada}x</span> ${nomeProduto} <span class="texto-azul">R$${precoProduto}</span></br>`;
@@ -17,12 +20,16 @@ function adicionar(){
 }
 
 function limpar(){
+    let quantidadeSelecionadaTexto = document.getElementById('quantidade');
     let selecionarTotal = document.getElementById('valor-total');
+    let carrinho = document.getElementById('lista-produtos');
+    let listaProdutos = carrinho.querySelector('.carrinho__produtos__produto')
+
+    quantidadeSelecionadaTexto.value = '';
     valorTotal = 0;
     selecionarTotal.innerHTML = `<span class="texto-azul" id="valor-total">R$${valorTotal}</span>`;
-    let carrinho = document.getElementById('lista-produtos');
-    let listaProdutos = carrinho.querySelector('.carrinho__produtos__produto');
-    listaProdutos.innerHTML = " ";
+    listaProdutos.innerHTML = ' ';
+
 }
 
 
