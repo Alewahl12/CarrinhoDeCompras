be3e772aee854a3ea2cd155c001e1d003086d972
Alexandre Wahl <alexandre.wahl12@gmail.com>
2026-02-14 00:00:04 -0300

    refactor: refatoracao e organizacao do codigo para melhor legibilidade

--- js/app.js
+++ js/app.js
@@ -1,36 +1,34 @@
 function adicionar(){
+    //Obter valores do produto (nome, preço, quantidade);
     let produtoSelecionado = document.getElementById('produto').value;
     let nomeProduto = produtoSelecionado.split(' - ')[0];
     let precoProduto = produtoSelecionado.split('R$')[1];
     let quantidadeSelecionada = document.getElementById('quantidade').value;
+
+    //Adicionar item no carrinho e calcular subtotal
     let carrinho = document.getElementById('lista-produtos');
-    let listaProdutos = carrinho.querySelector('.carrinho__produtos__produto');
+    carrinho.innerHTML += `<section class="carrinho__produtos__produto"><span class="texto-azul">${quantidadeSelecionada}x</span> ${nomeProduto} <span class="texto-azul">R$${precoProduto*quantidadeSelecionada}</span></section>`;
+
+    //Calcular valor total dos itens adicionados e exibir
     let selecionarTotal = document.getElementById('valor-total');
-    let quantidadeSelecionadaTexto = document.getElementById('quantidade');
-        
-    quantidadeSelecionadaTexto.value = '';
-
-
-    listaProdutos.innerHTML += `<br><span class="texto-azul">${quantidadeSelecionada}x</span> ${nomeProduto} <span class="texto-azul">R$${precoProduto}</span></br>`;
-    
     valorTotal += parseFloat(precoProduto) * quantidadeSelecionada;
-
     selecionarTotal.innerHTML = `<span class="texto-azul" id="valor-total">R$${valorTotal}</span>`;
 
+    //Limpar valor do campo quantidade ao clicar em adicionar
+    let quantidadeSelecionadaTexto = document.getElementById('quantidade');
+    quantidadeSelecionadaTexto.value = '';
 }
 
 function limpar(){
+    //Selecionar campos de quantidade, valor total e o carrinho de compras para "zerar"
     let quantidadeSelecionadaTexto = document.getElementById('quantidade');
     let selecionarTotal = document.getElementById('valor-total');
     let carrinho = document.getElementById('lista-produtos');
-    let listaProdutos = carrinho.querySelector('.carrinho__produtos__produto')
 
     quantidadeSelecionadaTexto.value = '';
     valorTotal = 0;
     selecionarTotal.innerHTML = `<span class="texto-azul" id="valor-total">R$${valorTotal}</span>`;
-    listaProdutos.innerHTML = ' ';
-
+    carrinho.innerHTML = ' ';
 }
-
 
 let valorTotal = 0;
